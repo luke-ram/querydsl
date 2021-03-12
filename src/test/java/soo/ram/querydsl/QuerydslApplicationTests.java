c0548124542e5b2632cd29a0d58ab374a2a186c4
luke-ram <sooramho@gmail.com>
2021-03-12 09:23:37 +0900

    querydsl 프로젝트 셋팅 완료

--- src/test/java/soo/ram/querydsl/QuerydslApplicationTests.java
+++ src/test/java/soo/ram/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,36 @@
 package soo.ram.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import soo.ram.querydsl.entity.Hello;
+import soo.ram.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = jpaQueryFactory
+                .selectFrom(qHello)
+                .fetchOne();
+        Assertions.assertThat(result).isEqualTo(hello);
     }
 
 }
