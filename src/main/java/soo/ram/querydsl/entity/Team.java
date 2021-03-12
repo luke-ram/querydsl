145ede38018830e146cb2e8c8328df780752bb2a
luke-ram <sooramho@gmail.com>
2021-03-12 09:47:06 +0900

    entity create and make testcase

---
+++ src/main/java/soo/ram/querydsl/entity/Team.java
@@ -0,0 +1,31 @@
+package soo.ram.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Team {
+
+
+    @Id
+    @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> member = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+}
