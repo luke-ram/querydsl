a744aa9551f7ca1ae8b16fa9caaf3deecf34c03e
luke-ram <ramhosoo@gmail.com>
2021-03-14 13:58:36 +0900

    api만들기

---
+++ src/main/java/soo/ram/querydsl/controller/MemberController.java
@@ -0,0 +1,22 @@
+package soo.ram.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import soo.ram.querydsl.dto.MemberSearchCondition;
+import soo.ram.querydsl.dto.MemberTeamDto;
+import soo.ram.querydsl.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
