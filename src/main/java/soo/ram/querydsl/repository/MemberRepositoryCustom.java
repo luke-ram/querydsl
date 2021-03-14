c8c96232bf6e444c51abc278b8e5e7b61c8908f2
sooram <ramhosoo@gmail.com>
2021-03-14 21:04:19 +0900

    페이징 연동

--- src/main/java/soo/ram/querydsl/repository/MemberRepositoryCustom.java
+++ src/main/java/soo/ram/querydsl/repository/MemberRepositoryCustom.java
@@ -1,5 +1,7 @@
 package soo.ram.querydsl.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import soo.ram.querydsl.dto.MemberSearchCondition;
 import soo.ram.querydsl.dto.MemberTeamDto;
@@ -9,4 +11,8 @@ import java.util.List;
 @Repository
 public interface MemberRepositoryCustom {
     List<MemberTeamDto> search(MemberSearchCondition condition);
+
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
 }
--- src/main/java/soo/ram/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/soo/ram/querydsl/repository/MemberRepositoryImpl.java
@@ -1,7 +1,11 @@
 package soo.ram.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import soo.ram.querydsl.dto.MemberSearchCondition;
 import soo.ram.querydsl.dto.MemberTeamDto;
 import soo.ram.querydsl.dto.QMemberTeamDto;
@@ -42,6 +46,69 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                         ageGoe(condition.getAgeGoe()),
                         ageLoe(condition.getAgeLoe()))
                 .fetch();
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberTeamDto> results = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.userName,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+        // querydsl이 알아서 토탈쿼리 요청함
+
+        List<MemberTeamDto> content = results.getResults();
+        long total = results.getTotal();
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> results = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.userName,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long total = queryFactory
+                .select(member)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq((condition.getUsername())),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                ).fetchCount();
+        //total쿼리를 내가 직접 호출함
+        //토탈쿼리에도 조인이 포함되지 않는다. 성능 최적화
+        return new PageImpl<>(results, pageable, total);
     }
 
     private BooleanExpression ageLoe(Integer ageLoe) {
